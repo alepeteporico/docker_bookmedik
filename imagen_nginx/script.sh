7f09b125d03b49d6b6ffadfb1434bb050e462631
alejandrogv <tojandro@gmail.com>
2022-06-05 16:42:10 +0200

    nginx

--- imagen_nginx/script.sh
+++ imagen_nginx/script.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+sleep 10
+
 mysql -u $BOOKMEDIK_USER --password=$BOOKMEDIK_PASS -h $HOST_DB $NAME_DB < /var/www/html/schema.sql
 
-/usr/sbin/apache2ctl -D FOREGROUND
+nginx -g "daemon off;"
